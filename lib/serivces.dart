d25ab81c7b7cfca57e67256f922e05d7abf52bbe
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-08-06 13:49:08 +0200

    update production api url

--- lib/serivces.dart
+++ lib/serivces.dart
@@ -1,10 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/foundation.dart';
 
 enum ApiService {
   GraphQLAPI(kDebugMode
       ? "http://localhost:3000/graphql"
       : 'https://derdevam.com/graphql'),
-  WebsocketAPI(kDebugMode ? "http://localhost:3001" : 'wss://cio.derdevam.com');
+  WebsocketAPI(
+      kDebugMode ? "http://localhost:3001" : 'https://cio.derdevam.com');
 
   final String path;
   const ApiService(this.path);
