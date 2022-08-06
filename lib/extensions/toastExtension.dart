51c7d42fea64432f22625701a63506e3734dacb7
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-08-06 13:13:52 +0200

    Create toast extension on top of buildcontext
    For creating much developer friendly, clean code structure. #1

---
+++ lib/extensions/toastExtension.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../theme/toast.dart';
+
+extension ToastExtension on BuildContext {
+  Toast get toast => const Toast();
+}
---
+++ lib/theme/toast.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:scflutter/main.dart';
+import 'package:scflutter/utils/palette.dart';
+
+enum ToastType {
+  Error(ColorPalette.red),
+  Success(ColorPalette.green),
+  Info(ColorPalette.primary);
+
+  final Color color;
+  const ToastType(this.color);
+}
+
+class Toast {
+  const Toast();
+
+  showToast(String text, {ToastType? toastType = ToastType.Info}) {
+    scaffoldKey.currentState?.showSnackBar(SnackBar(
+      content: Text(text),
+      backgroundColor: toastType?.color ?? ColorPalette.primary,
+    ));
+  }
+}
