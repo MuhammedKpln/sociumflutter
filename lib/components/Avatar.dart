b6731090f65df5a55e961abc37359f071f2b64b4
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-08-06 12:39:44 +0200

    use enchanced enums for avatar sizes

--- lib/components/Avatar.dart
+++ lib/components/Avatar.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:scflutter/utils/palette.dart';
 
-enum AvatarSize { extraSmall, small, medium, mediumish, large }
+enum AvatarSize {
+  extraSmall(20),
+  small(30),
+  mediumish(40),
+  medium(50),
+  large(100);
+
+  final double size;
+
+  const AvatarSize(this.size);
+}
 
 class Avatar extends StatelessWidget {
   const Avatar(
@@ -13,37 +23,11 @@ class Avatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double? width;
-    double? height;
-
     final bool isLargeAvatar = avatarSize == AvatarSize.large;
 
-    switch (avatarSize) {
-      case AvatarSize.extraSmall:
-        width = 20;
-        height = 20;
-        break;
-      case AvatarSize.small:
-        width = 30;
-        height = 30;
-        break;
-      case AvatarSize.mediumish:
-        width = 40;
-        height = 40;
-        break;
-      case AvatarSize.medium:
-        width = 50;
-        height = 50;
-        break;
-      case AvatarSize.large:
-        width = 100;
-        height = 100;
-        break;
-    }
-
     return Container(
-      width: width,
-      height: height,
+      width: avatarSize.size,
+      height: avatarSize.size,
       decoration: BoxDecoration(
           gradient: ColorPalette.bgLinearGradient,
           borderRadius: avatarSize != AvatarSize.extraSmall
