05de031db36c738eebc799126ce88e4cebc0e6c2
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-09-06 16:59:01 +0200

    add shimmer to profile #12

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -3,9 +3,10 @@ import 'package:feather_icons/feather_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scflutter/components/Avatar.dart';
-import 'package:scflutter/components/Loading.dart';
+import 'package:scflutter/components/LoadingNew.dart';
 import 'package:scflutter/components/RoundedButton.dart';
 import 'package:scflutter/components/Scaffold.dart';
+import 'package:scflutter/mixins/NewLoading.mixin.dart';
 import 'package:scflutter/models/follower.dart';
 import 'package:scflutter/models/user.dart';
 import 'package:scflutter/repositories/follower.repository.dart';
@@ -14,7 +15,6 @@ import 'package:scflutter/state/auth.state.dart';
 import 'package:scflutter/utils/palette.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../mixins/Loading.mixin.dart';
 import '../utils/router.gr.dart';
 
 class ProfilePage extends ConsumerStatefulWidget {
@@ -26,11 +26,12 @@ class ProfilePage extends ConsumerStatefulWidget {
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
+class _ProfileScreenState extends ConsumerState<ProfilePage>
+    with NewLoadingMixin<ProfilePage>, TickerProviderStateMixin {
   final UserRepository _userRepository = UserRepository();
   final FollowerRepository _followerRepository = FollowerRepository();
-  late UserModel user;
-  late FollowerRepositoryOutput followers;
+  UserModel? user;
+  FollowerRepositoryOutput? followers;
 
   void onPressSettings() {
     context.router.navigate(const ProfileSettingsScreenRoute());
@@ -40,8 +41,8 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
     _userRepository.fetchUser(widget.username).then((value) async {
       final followersOutput = await fetchUserFollowers();
 
+      setLoading(false);
       setState(() {
-        isLoading = false;
         followers = followersOutput;
         user = value;
       });
@@ -62,10 +63,6 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
 
   @override
   Widget build(BuildContext context) {
-    if (isLoading) {
-      return const Loading();
-    }
-
     return AppScaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -100,28 +97,35 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
                         child: Column(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 5),
-                                    child: Text(user.username),
-                                  ),
-                                  Container(
-                                    width: 10,
-                                    height: 10,
-                                    decoration: BoxDecoration(
-                                        color: ColorPalette.green,
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                  )
-                                ],
+                              Loading(
+                                type: LoadingType.singleLine,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 5),
+                                      child:
+                                          Text(user?.username ?? "No username"),
+                                    ),
+                                    Container(
+                                      width: 10,
+                                      height: 10,
+                                      decoration: BoxDecoration(
+                                          color: ColorPalette.green,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                    )
+                                  ],
+                                ),
                               ),
-                              Text(
-                                //FIXME: implement
-                                user.biography ??
-                                    "Herhangi bir bilgi bulunmamakta.",
-                                style: const TextStyle(color: Colors.grey),
+                              Loading(
+                                type: LoadingType.singleLine,
+                                child: Text(
+                                  //FIXME: implement
+                                  user?.biography ??
+                                      "Herhangi bir bilgi bulunmamakta.",
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
                               ),
                               //FIXME: implement
                               userFollowerInformation(followers),
@@ -185,7 +189,7 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
     );
   }
 
-  Row userFollowerInformation(FollowerRepositoryOutput data) {
+  Row userFollowerInformation(FollowerRepositoryOutput? data) {
     void navigateToFollowersPage({required bool fetchingFollowers}) {
       context.router
           .navigate(FollowersRoute(fetchingFollowers: fetchingFollowers));
@@ -204,11 +208,15 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
                     color: Colors.grey,
                     fontSize: Theme.of(context).textTheme.titleSmall?.fontSize),
               ),
-              Text(
-                data.followings.round().toString(),
-                style: TextStyle(
-                    color: const Color(0xff8A84E2),
-                    fontSize: Theme.of(context).textTheme.titleLarge?.fontSize),
+              Loading(
+                type: LoadingType.singleLine,
+                child: Text(
+                  data?.followings.round().toString() ?? "0",
+                  style: TextStyle(
+                      color: const Color(0xff8A84E2),
+                      fontSize:
+                          Theme.of(context).textTheme.titleLarge?.fontSize),
+                ),
               )
             ],
           ),
@@ -223,11 +231,15 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
                     color: Colors.grey,
                     fontSize: Theme.of(context).textTheme.titleSmall?.fontSize),
               ),
-              Text(
-                data.followers.round().toString(),
-                style: TextStyle(
-                    color: const Color(0xffF9A620),
-                    fontSize: Theme.of(context).textTheme.titleLarge?.fontSize),
+              Loading(
+                type: LoadingType.singleLine,
+                child: Text(
+                  data?.followers.round().toString() ?? "0",
+                  style: TextStyle(
+                      color: const Color(0xffF9A620),
+                      fontSize:
+                          Theme.of(context).textTheme.titleLarge?.fontSize),
+                ),
               )
             ],
           ),
@@ -240,11 +252,14 @@ class _ProfileScreenState extends ConsumerState<ProfilePage> with LoadingMixin {
                   color: Colors.grey,
                   fontSize: Theme.of(context).textTheme.titleSmall?.fontSize),
             ),
-            Text(
-              "32",
-              style: TextStyle(
-                  color: const Color(0xffD4AFCD),
-                  fontSize: Theme.of(context).textTheme.titleLarge?.fontSize),
+            Loading(
+              type: LoadingType.singleLine,
+              child: Text(
+                "32",
+                style: TextStyle(
+                    color: const Color(0xffD4AFCD),
+                    fontSize: Theme.of(context).textTheme.titleLarge?.fontSize),
+              ),
             )
           ],
         ),
