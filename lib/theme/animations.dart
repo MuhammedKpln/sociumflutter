0dca23b2e878745646d28921d375da4c0d30106d
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-09-10 00:30:10 +0200

    update lottie

--- lib/theme/animations.dart
+++ lib/theme/animations.dart
@@ -1,7 +1,8 @@
 enum Animations {
   search("assets/animations/search.json"),
   loading("assets/animations/loading.json"),
-  confetti("assets/animations/confetti.json");
+  confetti("assets/animations/confetti.json"),
+  loadingDots("assets/animations/loading-dots.json");
 
   final String path;
   const Animations(this.path);
