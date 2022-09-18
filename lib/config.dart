a54532884005584a66d64a43ba9198f6b5848dd7
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-09-18 19:04:39 +0200

    Use envied package Fixes #21

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,8 @@
+import 'package:scflutter/utils/env.dart';
+
 class Config {
   static DateTime maximumDateForRegistration =
       DateTime(DateTime.now().year - 18, 1, 1);
   static DateTime minimumDateForRegistration = DateTime(1980);
-  static const String SupabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqaWZoZHBueG13YnVhb3h1bWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA5MDY5NjAsImV4cCI6MTk3NjQ4Mjk2MH0.SuzFrQx8V9hYz26smlsvXnPVuriSoivmCuF-5ION9yk";
+  static const String SupabaseAnonKey = Env.SUPABASE_ANON_KEY;
 }
---
+++ lib/utils/env.dart
@@ -0,0 +1,13 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: ".env")
+abstract class Env {
+  @EnviedField()
+  static const SUPABASE_URL = _Env.SUPABASE_URL;
+  @EnviedField()
+  static const SUPABASE_ANON_KEY = _Env.SUPABASE_ANON_KEY;
+  @EnviedField()
+  static const WEBSOCKET_API = _Env.WEBSOCKET_API;
+}
