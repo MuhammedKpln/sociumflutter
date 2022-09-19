bd22ff8d39cddf8393f83282adaf84841814dfb5
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-09-19 19:40:26 +0200

    fix envied static

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:scflutter/utils/env.dart';
 
 class Config {
   static DateTime maximumDateForRegistration =
       DateTime(DateTime.now().year - 18, 1, 1);
   static DateTime minimumDateForRegistration = DateTime(1980);
-  static const String SupabaseAnonKey = Env.SUPABASE_ANON_KEY;
+  static final String SupabaseAnonKey = Env.SUPABASE_ANON_KEY;
 }
--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -4,10 +4,10 @@ part 'env.g.dart';
 
 @Envied(path: ".env")
 abstract class Env {
-  @EnviedField(obfuscate: true)
+  @EnviedField()
   static const SUPABASE_URL = _Env.SUPABASE_URL;
   @EnviedField(obfuscate: true)
-  static const SUPABASE_ANON_KEY = _Env.SUPABASE_ANON_KEY;
-  @EnviedField(obfuscate: true)
+  static final SUPABASE_ANON_KEY = _Env.SUPABASE_ANON_KEY;
+  @EnviedField()
   static const WEBSOCKET_API = _Env.WEBSOCKET_API;
 }
