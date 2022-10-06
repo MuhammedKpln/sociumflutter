414f2c04c879f782bc8b84c9a107f6db6459cce5
Cirrus CI <support@cirruslabs.org>
2022-10-06 04:50:01 +0000

    Supabase 1.0.0 #31

--- lib/repositories/auth.repository.dart
+++ lib/repositories/auth.repository.dart
@@ -1,61 +1,64 @@
 import 'package:scflutter/models/login_response.model.dart';
-import 'package:scflutter/utils/logger.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:scflutter/repositories/_base.dart';
 
 import '../models/user.dart';
 
-class AuthRepository with LoggerMixin {
-  final SupabaseClient _supabase = Supabase.instance.client;
-
+class AuthRepository extends BaseRepositoryClass {
   Future<LoginResponse> signIn(
       {required String email, required String password}) async {
-    final request =
-        await _supabase.auth.signIn(email: email, password: password);
+    try {
+      final request =
+          await supabase.auth.signIn(email: email, password: password);
 
-    if (request.session == null) {
-      throw Exception(request.statusCode);
+      final userProfile =
+          await getUserProfile(userUuid: request.user?.id ?? "");
+
+      return LoginResponse(
+          user: userProfile,
+          rawUser: request.user!,
+          accessToken: request.session!.accessToken,
+          refreshToken: request.session!.refreshToken!);
+    } catch (error) {
+      logError(error);
+      throw Exception(error);
     }
-
-    final userProfile = await getUserProfile(userUuid: request.user?.id ?? "");
-
-    return LoginResponse(
-        user: userProfile,
-        rawUser: request.user!,
-        accessToken: request.session!.accessToken,
-        refreshToken: request.session!.refreshToken!);
   }
 
   Future<void> signOut() async {
-    await _supabase.auth.signOut();
+    await supabase.auth.signOut();
   }
 
   Future<LoginResponse> signUp(
       {required String email,
       required String password,
       required String username}) async {
-    final request = await _supabase.auth
-        .signUp(email, password, userMetadata: {"username": username});
+    try {
+      final request = await supabase.auth
+          .signUp(email, password, userMetadata: {"username": username});
 
-    if (request.session == null) {
-      throw Exception(request.statusCode);
+      final userProfile =
+          await getUserProfile(userUuid: request.user?.id ?? "");
+
+      return LoginResponse(
+          user: userProfile,
+          rawUser: request.user!,
+          accessToken: request.session!.accessToken,
+          refreshToken: request.session!.refreshToken!);
+    } catch (error) {
+      logError(error);
+      throw Exception(error);
     }
-
-    final userProfile = await getUserProfile(userUuid: request.user?.id ?? "");
-    return LoginResponse(
-        user: userProfile,
-        rawUser: request.user!,
-        accessToken: request.session!.accessToken,
-        refreshToken: request.session!.refreshToken!);
   }
 
   Future<UserModel> getUserProfile({required String userUuid}) async {
-    final request =
-        await _supabase.from('users').select().eq('id', userUuid).single();
+    try {
+      final request =
+          await supabase.from('users').select().eq('id', userUuid).single();
 
-    if (request == null) {
-      logError("No profile found with userId: $userUuid");
+      return UserModel.fromJson(request);
+    } catch (error) {
+      logError(error);
+      throw Exception(error);
     }
-
-    return UserModel.fromJson(request.data);
   }
 }
